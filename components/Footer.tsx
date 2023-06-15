18035c751bb084150bf4e72b83ea565c8cc38aae
Pedro Arruda <pedroarruda91@gmail.com>
2023-06-15 17:04:21 -0300

    footer refac

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,54 +8,39 @@ interface SocialButtonProps {
   href: string;
 }
 
-const SocialButton = ({ children, label, href }: SocialButtonProps) => {
-  return (
-    <chakra.button
-      bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
-      rounded={'full'}
-      w={8}
-      h={8}
-      cursor={'pointer'}
-      as={'a'}
-      href={href}
-      display={'inline-flex'}
-      alignItems={'center'}
-      justifyContent={'center'}
-      transition={'background 0.3s ease'}
-      _hover={{
-        bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
-      }}
-    >
-      <VisuallyHidden>{label}</VisuallyHidden>
-      {children}
-    </chakra.button>
-  );
-};
+const SocialButton = ({ children, label, href }: SocialButtonProps) => (
+  <chakra.button
+    bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+    rounded="full"
+    w={8}
+    h={8}
+    cursor="pointer"
+    as="a"
+    href={href}
+    display="inline-flex"
+    alignItems="center"
+    justifyContent="center"
+    transition="background 0.3s ease"
+    _hover={{ bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200') }}
+  >
+    <VisuallyHidden>{label}</VisuallyHidden>
+    {children}
+  </chakra.button>
+);
 
 export default function Footer() {
   return (
-    <Box
-      bg={useColorModeValue('gray.50', 'gray.900')}
-      color={useColorModeValue('gray.700', 'gray.200')}
-    >
-      <Container
-        as={Stack}
-        maxW={'6xl'}
-        py={4}
-        direction={{ base: 'column', md: 'row' }}
-        spacing={4}
-        justify={{ base: 'center', md: 'space-between' }}
-        align={{ base: 'center', md: 'center' }}
-      >
+    <Box bg={useColorModeValue('gray.50', 'gray.900')} color={useColorModeValue('gray.700', 'gray.200')}>
+      <Container as={Stack} maxW="6xl" py={4} direction={{ base: 'column', md: 'row' }} spacing={4} justify={{ base: 'center', md: 'space-between' }} align={{ base: 'center', md: 'center' }}>
         <Text>© 2023 Pedro Arruda. Todos os direitos reservados.</Text>
-        <Stack direction={'row'} spacing={6}>
-          <SocialButton label={'Twitter'} href={'#'}>
+        <Stack direction="row" spacing={6}>
+          <SocialButton label="Twitter" href="#">
             <FaTwitter />
           </SocialButton>
-          <SocialButton label={'YouTube'} href={'#'}>
+          <SocialButton label="YouTube" href="#">
             <FaYoutube />
           </SocialButton>
-          <SocialButton label={'Instagram'} href={'#'}>
+          <SocialButton label="Instagram" href="#">
             <FaInstagram />
           </SocialButton>
         </Stack>
